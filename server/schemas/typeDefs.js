e54d5b0d5eb0876f811ced89963414cb2b9a1cbb
Hybee <hybee234@outlook.com>
2024-01-30 04:21:40 +1100

    graphQL dates ....

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,9 @@
+const { GraphQLScalarType, Kind } = require("graphql");
+//import dateScalar from './scalers/date'
+
 const typeDefs = `
+scalar Date  #HL
+
     type User {
         _id: ID
         username: String
@@ -9,12 +14,12 @@ const typeDefs = `
 
     type Task {
         _id: ID
-        # created_dt: Date
+        created_dt: Date
         title: String
         summary: String
         complete_flag: Boolean
-        # complete_dt: Date
-        # remind_dt: Date
+        complete_dt: Date
+        remind_dt: Date
         stakeholder: String
         assigned: String
         status_macro: String
@@ -27,7 +32,7 @@ const typeDefs = `
         noteId: ID
         note: String
         author: String
-        # note_dt: Date
+        note_dt: Date
     }
 
     type Priority {
@@ -52,14 +57,41 @@ const typeDefs = `
     }
 
     # type Mutation {
-    #     createUser(username: String! email: String! password: String!): Auth
-    #     login(username: String email: String password: String!): Auth
-    #     saveBook(book:saveBookInput): User
-    #     deleteBook (bookId: String! ): User
-    #     deleteBookGraphQl (bookId: String! ): User
+    #    createUser(username: String! email: String! password: String!): Auth
+    #    login(username: String email: String password: String!): Auth
+    #    saveBook(book:saveBookInput): User
+    #    deleteBook (bookId: String! ): User
+    #    deleteBookGraphQl (bookId: String! ): User
     # }
 
-
 `;
+
+// Custom Scalar to define Date
+// const dateScalar = new GraphQLScalarType({
+//     name: 'Date',
+//     description: 'Date custom scalar type',
+//     serialize(value) {
+//         if (value instanceof Date) {
+//             return value.getTime(); // Convert outgoing Date to integer for JSON
+//         }
+//         throw Error('GraphQL Date Scalar serializer expected a `Date` object');
+//     },
+
+//     parseValue(value) {
+//         if (typeof value === 'number') {
+//             return new Date(value); // Convert incoming integer to Date
+//         }
+//         throw new Error('GraphQL Date Scalar parser expected a `number`');
+//     },
+
+//     parseLiteral(ast) {
+//         if (ast.kind === Kind.INT) {
+//             // Convert hard-coded AST string to integer and then to Date
+//             return new Date(parseInt(ast.value, 10));
+//         }        
+//         // Invalid hard-coded value (not an integer)
+//         return null;
+//     },
+// });
 
 module.exports = typeDefs;
