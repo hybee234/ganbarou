0ccaade6532a14b33916c31da6913d15f220bdb9
Hybee <hybee234@outlook.com>
2024-02-04 14:44:26 +1100

    styling changes, redesigned logo, responsive design for welcome page

--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -21,25 +21,26 @@ export default function Welcome() {
                 <h1>Welcome to Ganbarou!</h1>
             </div>
             
-            <div className = "about-me-page bg-filter justify-center mt-10">
+            <div className = "welcome-page bg-filter justify-center">
                 {/* <h2 className="w-full"> A team task management solution </h2>                 */}
                 <div className ="flex flex-wrap justify-center">
                     <div className = "flex p-2 m-2 justify-start">
-                        <img src={picture} width="500px" className="about-me-profile-picture m-auto"></img>                                 
+                        <img src='./assets/images/Ganbarou Team 3.jpeg' width="500px" className="welcome-picture m-auto"></img>                                 
                     </div>
-                        <div className = "about-me-text-container p-2 m-2 text-center">               
-                        <p> "Ganbarou" (頑張ろう) is a Japanese expression of encouragement and support in times of challenge. It means "Let's do our best together!", calling on the teams determination, perseverence, and collaboration. </p>       
-                        <p> Ganbarou seeks to support the positive team spirit by providing a means to organise and plan team tasks. </p>  
+                        <div className = "welcome-text-container p-2 m-2 text-center">               
+                        <p> "Ganbarou!" is a Japanese expression of encouragement and support in times of challenge.</p>
+                        <p> Ganbarou means "Lets do our best together!" inspiring team determination, perseverence, and collaboration. </p>       
+                        <p> My solution seeks to support the positive team spirit by providing a means to organise and plan team tasks! </p>  
                         {
                             Auth.loggedIn() ?
                             (
-                                <div className="text-center pt-10">                            
-                                    <Link to="/MyTasks" className="button-color w-28 px-6 py-2 text-2xl">View My Tasks</Link>                      
+                                <div className="text-center">                            
+                                    <Link to="/MyTasks" className="button-color w-28 px-6 py-2">View My Tasks</Link>                      
                                 </div>
                             ) : (                              
-                                <div className="text-center pt-10">                            
-                                    <Link to="/Login" className="button-color w-28 px-6 py-2 text-2xl">Login</Link>&nbsp; or &nbsp;
-                                    <Link to="/Signup" className="button-color w-28 px-6 py-2 text-2xl">Sign Up</Link>                            
+                                <div className="text-center">                            
+                                    <Link to="/Login" className="button-color w-28 px-6 py-2">Login</Link>&nbsp; or &nbsp;
+                                    <Link to="/Signup" className="button-color w-28 px-6 py-2">Sign Up</Link>                            
                                 </div>
                             )
                         }
