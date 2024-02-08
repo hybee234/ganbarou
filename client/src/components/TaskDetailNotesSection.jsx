5a70a5d2b76e733a538564e1de2e524e7789b431
Hybee <hybee234@outlook.com>
2024-02-08 14:59:19 +1100

    set up frame for notes section

--- client/src/components/TaskDetailNotesSection.jsx
+++ client/src/components/TaskDetailNotesSection.jsx
@@ -2,12 +2,12 @@ export default function TaskDetailNotesSection( ) {
 
     return (
         <div className="modal-section bg-filter backdrop:p-5">
-            <label className="modal-label w-full  text-right"> Notes Section</label>
-
-            <div className="modal-section-divider ">
-                <div className="m-auto">
-                            Test
-                    <div className="w-full">
+            <label className="modal-label w-full"> Notes Section</label>
+            <div className="modal-section-divider "> Modal Section Divider
+                <div className="modal-field-container">
+                    <label className="modal-label w-1/3">Modal Label</label>
+                    <div className = "modal-field">
+                        Modal Field
                     </div>
                 </div>
             </div>
